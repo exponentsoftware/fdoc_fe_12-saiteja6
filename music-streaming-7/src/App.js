bb084ff9484eb40727dacf64eab8a2530cb02724
saiteja6 <bendalamsaiteja6@gmail.com>
2021-03-15 19:18:22 +0530

    added new components and installed antd

--- music-streaming-7/src/App.js
+++ music-streaming-7/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
+import AddAlbum from './components/AddAlbum';
+import Album from './components/Album';
+import Albums from './components/Albums'
+
 
 function App() {
+  const [artist, setArtist] = useState('A.R. Rahman')
+  const [album, setAlbum] = useState('Rockstar')
+  const [albumCover, setAlbumCover] = useState('https://upload.wikimedia.org/wikipedia/en/2/2b/Rockstar_%28soundtrack%29.jpg')
+
+
+  const [albums, setAlbums] = useState([{id: 1, aritst: 'A.R. Rahman', album_title: 'Rockstar', album_cover: 'https://upload.wikimedia.org/wikipedia/en/2/2b/Rockstar_%28soundtrack%29.jpg', songs: ['Kun Fayakun', 'Nadaan Parindey', 'Saadda Haq']},
+  {id: 2, aritst: 'A.R. Rahman', album_title: 'Highway', album_cover: 'https://a10.gaanacdn.com/images/albums/86/150486/crop_175x175_150486.jpg', songs: ['Patakha Guddi - Female Version', 'Maahi Ve']},
+  {id: 3, aritst: 'Vishal-Shekhar', album_title: 'I hate Luv Storys', album_cover: 'https://a10.gaanacdn.com/images/albums/80/22080/crop_175x175_22080.jpg', songs: ['Bahara', 'Bin Tere', 'I hate Luv Storys', 'Sadka Hua', 'Jab Mila Tu']}])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Album artist={artist} album={album} albumCover={albumCover}/>
+      <Albums albums={albums} />
+      <AddAlbum />
     </div>
   );
 }
