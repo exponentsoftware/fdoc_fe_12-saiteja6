3770dc726b11ce06dc6899ef6c9928c224ddd31e
saiteja6 <bendalamsaiteja6@gmail.com>
2021-03-15 20:51:36 +0530

    mapped over the props data

--- music-streaming-7/src/App.js
+++ music-streaming-7/src/App.js
@@ -1,27 +1,66 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css';
-import AddAlbum from './components/AddAlbum';
-import Album from './components/Album';
+// import AddAlbum from './components/AddAlbum';
+// import Album from './components/Album';
 import Albums from './components/Albums'
 
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        // aritst: 'A.R. Rahman',
+        // album: 'Rockstar',
+        // albumCover: 'https://upload.wikimedia.org/wikipedia/en/2/2b/Rockstar_%28soundtrack%29.jpg',
+        albums: [
+        { id: 1,
+          aritst: 'A.R. Rahman', 
+          album_title: 'Rockstar', 
+          album_cover: 'https://upload.wikimedia.org/wikipedia/en/2/2b/Rockstar_%28soundtrack%29.jpg', 
+          songs: ['Kun Fayakun', 'Nadaan Parindey', 'Saadda Haq']
+        },
+        { id: 2, 
+          aritst: 'A.R. Rahman', 
+          album_title: 'Highway', 
+          album_cover: 'https://a10.gaanacdn.com/images/albums/86/150486/crop_175x175_150486.jpg', 
+          songs: ['Patakha Guddi - Female Version', 'Maahi Ve']
+        },
+        { id: 3, 
+          aritst: 'Vishal-Shekhar', 
+          album_title: 'I hate Luv Storys', 
+          album_cover: 'https://a10.gaanacdn.com/images/albums/80/22080/crop_175x175_22080.jpg', 
+          songs: ['Bahara', 'Bin Tere', 'I hate Luv Storys', 'Sadka Hua', 'Jab Mila Tu']
+        }
+      ]
+      }
+    }
 
-function App() {
-  const [artist, setArtist] = useState('A.R. Rahman')
-  const [album, setAlbum] = useState('Rockstar')
-  const [albumCover, setAlbumCover] = useState('https://upload.wikimedia.org/wikipedia/en/2/2b/Rockstar_%28soundtrack%29.jpg')
-
-
-  const [albums, setAlbums] = useState([{id: 1, aritst: 'A.R. Rahman', album_title: 'Rockstar', album_cover: 'https://upload.wikimedia.org/wikipedia/en/2/2b/Rockstar_%28soundtrack%29.jpg', songs: ['Kun Fayakun', 'Nadaan Parindey', 'Saadda Haq']},
-  {id: 2, aritst: 'A.R. Rahman', album_title: 'Highway', album_cover: 'https://a10.gaanacdn.com/images/albums/86/150486/crop_175x175_150486.jpg', songs: ['Patakha Guddi - Female Version', 'Maahi Ve']},
-  {id: 3, aritst: 'Vishal-Shekhar', album_title: 'I hate Luv Storys', album_cover: 'https://a10.gaanacdn.com/images/albums/80/22080/crop_175x175_22080.jpg', songs: ['Bahara', 'Bin Tere', 'I hate Luv Storys', 'Sadka Hua', 'Jab Mila Tu']}])
-
-  return (
-    <div className="App">
-      <Album artist={artist} album={album} albumCover={albumCover}/>
-      <Albums albums={albums} />
-      <AddAlbum />
-    </div>
-  );
+    render() {
+      return(
+        <div className="App">
+            {/* <Album artist={this.state.artist} album={this.state.album} albumCover={this.state.albumCover}/> */}
+            <Albums albums={this.state.albums} />
+            {/* <AddAlbum /> */}
+        </div>
+      )
+    }
 }
+// function App() {
+//   const [artist, setArtist] = useState('A.R. Rahman')
+//   const [album, setAlbum] = useState('Rockstar')
+//   const [albumCover, setAlbumCover] = useState('https://upload.wikimedia.org/wikipedia/en/2/2b/Rockstar_%28soundtrack%29.jpg')
+
+
+//   const [albums, setAlbums] = useState([{id: 1, aritst: 'A.R. Rahman', album_title: 'Rockstar', album_cover: 'https://upload.wikimedia.org/wikipedia/en/2/2b/Rockstar_%28soundtrack%29.jpg', songs: ['Kun Fayakun', 'Nadaan Parindey', 'Saadda Haq']},
+//   {id: 2, aritst: 'A.R. Rahman', album_title: 'Highway', album_cover: 'https://a10.gaanacdn.com/images/albums/86/150486/crop_175x175_150486.jpg', songs: ['Patakha Guddi - Female Version', 'Maahi Ve']},
+//   {id: 3, aritst: 'Vishal-Shekhar', album_title: 'I hate Luv Storys', album_cover: 'https://a10.gaanacdn.com/images/albums/80/22080/crop_175x175_22080.jpg', songs: ['Bahara', 'Bin Tere', 'I hate Luv Storys', 'Sadka Hua', 'Jab Mila Tu']}])
+
+//   return (
+//     <div className="App">
+//       <Album artist={artist} album={album} albumCover={albumCover}/>
+//       <Albums albums={albums} />
+//       <AddAlbum />
+//     </div>
+//   );
+// }
 
 export default App;
--- music-streaming-7/src/components/Albums.js
+++ music-streaming-7/src/components/Albums.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import Album from './Album'
+// import Album from './Album'
 
 export default function Albums(props) {
+    // console.log(props);
     return (
-        <div>
-{props.albums.map(album =>  <Album mappedData={album} />)}
+        <div class='container'>
+{props.albums.map(album => {
+    return( <ul>
+                    <img alt="example" src={album.album_cover} width="300" height="270" />
+                    <li key="{album}">{album.id}</li>
+                    <li>{album.artist}</li>
+                    <li>{album.album_title}</li>
+                    {/* <li>{album.album_cover}</li> */}
+
+
+                    </ul> )})}
         </div>
     )
-}
+    }
+
